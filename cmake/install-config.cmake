8c07a4e6b5f4a3979668b115a952c4aeaf5c489d
Zoe Nickson <mjn6@st-andrews.ac.uk>
2025-10-10 16:10:35 +0100

    Initialised the repository (#1)
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>

---
+++ cmake/install-config.cmake
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2025 https://github.com/friendlyanon
+# SPDX-License-Identifier: Unlicense
+
+set(citescoop-proto_FOUND YES)
+
+include(CMakeFindDependencyMacro)
+find_dependency(fmt)
+
+if(citescoop-proto_FOUND)
+  include("${CMAKE_CURRENT_LIST_DIR}/citescoop-protoTargets.cmake")
+endif()
---
+++ cmake/prelude.cmake
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2025 https://github.com/friendlyanon
+# SPDX-License-Identifier: Unlicense
+
+# ---- In-source guard ----
+
+if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
+  message(
+      FATAL_ERROR
+      "In-source builds are not supported. "
+      "Please read the BUILDING document before trying to build this project. "
+      "You may need to delete 'CMakeCache.txt' and 'CMakeFiles/' first."
+  )
+endif()
